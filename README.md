bb4ab5b675d80d32ea3a5ed1f7776480aa922315
Taeho Hwang <hth225@gmail.com>
2021-05-18 20:30:27 +0900

    Initial commit

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # swift_movie_app
 simple movie app
+
+1. Use Clean architecture
+2. Use "The movie database" API
+3. **consider extensibility**
---
+++ swift_movie_app/swift_movie_appApp.swift
@@ -0,0 +1,17 @@
+//
+//  swift_movie_appApp.swift
+//  swift_movie_app
+//
+//  Created by Jason Hwang on 2021/05/18.
+//
+
+import SwiftUI
+
+@main
+struct swift_movie_appApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
